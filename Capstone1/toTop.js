8c60f1b8d850bd2d9c052e18a647ddcc3f755bb0
ricjamp023 <ricjamp@gmail.com>
2018-02-19 00:32:42 +0800

    Finished Capstone 1

--- Capstone1/toTop.js
+++ Capstone1/toTop.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// Show or hide the sticky footer button
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 200) {
-			$('.go-top').fadeIn(200);
+			$('.go-top').fadeIn(150);
 		} else {
 			$('.go-top').fadeOut(200);
 		}
